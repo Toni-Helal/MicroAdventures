1a0072591cec4891cd43195f1f72e94902f40cbe
Antoun Helal <pixel.forge.palace@gmail.com>
2026-02-05 19:06:17 +0100

    Initial Commit

---
+++ MicroAdventures/MicroAdventuresApp.swift
@@ -0,0 +1,17 @@
+//
+//  MicroAdventuresApp.swift
+//  MicroAdventures
+//
+//  Created by Antoun Helal on 05/02/2026.
+//
+
+import SwiftUI
+
+@main
+struct MicroAdventuresApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
